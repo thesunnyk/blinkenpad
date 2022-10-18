452f25002f9a8e28709033fdfafaf31a3ffa2430
Sunny Kalsi <thesunnyk@gmail.com>
2022-10-18 20:54:21 +1100

    add Cargo and clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate alsa;
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Blinkenpad")
+        .version("0.1.0")
+        .author("Sunny Kalsi <thesunnyk@gmail.com>")
+        .about("Blinkenlights and macropad on the Launchpad")
+        .get_matches();
+
 }
